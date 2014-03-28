fa2a627c57bc5262134ce1f75a3462cad7ea5f5d
Arnaldo Escossio <arnaldoescossio@gmail.com>
2014-03-27 23:50:31 -0300

    Criacao do projeto vota_prato em Ruby on Rails.

---
+++ app/models/restaurante.rb
@@ -0,0 +1,18 @@
+class Restaurante < ActiveRecord::Base
+  validates_presence_of :nome, :message => "deve ser preenchido"
+  validates_presence_of :endereco, :message => "deve ser preenchido"
+  validates_presence_of :especialidade, :message => "deve ser preenchida"
+  validates_uniqueness_of :nome, :message => "já cadastrado"
+  validates_uniqueness_of :endereco, :message => "já cadastrado"
+
+  validate :primeira_letra_deve_ser_maiuscula
+
+  has_many :qualificacoes
+  has_and_belongs_to_many :pratos
+  has_many :comentarios, :as => :comentavel
+
+  private
+  def primeira_letra_deve_ser_maiuscula
+    errors.add(:nome, "primeira letra deve ser maiúscula.") unless nome =~ /[A-Z].*/
+  end
+end
